04910bd232fba15706b1b3b4667d650455e8d7aa
ayoubleass <ayoubleas@gmail.com>
2023-12-16 03:14:08 -0800

    0x19. C - Stacks, Queues - LIFO, FIFO

--- pall.c
+++ pall.c
@@ -8,13 +8,15 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *current = *stack;
+
 	(void)line_number;
 	if (stack == NULL)
 		return;
 
-	while (*stack != NULL)
+	while (current != NULL)
 	{
-		printf("%d\n", (*stack)->n);
-		*stack = (*stack)->next;
+		printf("%d\n", current->n);
+		current = current->next;
 	}
 }
